ae039ab115874baaaafd78cd666cb74800ddf781
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-06-26 18:18:39 -0500

    método de agregar solucionado

--- arbol_binario.py
+++ arbol_binario.py
@@ -6,36 +6,59 @@ class nodo:
         self.right=right
 
 class BinaryTree:
-    def __init__(self,root) -> None:
-        self.root=root
+    def __init__(self) -> None:
+        self.root=None
         self.height=0  ##altura
         self.weight=0  ##peso
         self.order=0
 
-    def search(self,element:any,current_nodo=None)->nodo:
-        if not(current_nodo):
-            current_nodo=self.root
+    def search(self,element:any,current_nodo:nodo)->nodo:
         if current_nodo.element==element:
             return current_nodo
-        left=self.search(element,current_nodo.left)
-        if left:
-            return left
-        right=self.search(element,current_nodo.right)
-        if right:
-            return right
+        if  current_nodo.left:
+            left=self.search(element,current_nodo.left)
+            if left:
+             return left
+        if  current_nodo.right:
+            right=self.search(element,current_nodo.right)
+            if right:
+                return right
+        return None
+        
+    def _recorrido_preorden(self,node:nodo):
+        if node is not None:
+            print(node.element)
+            self._recorrido_preorden(node.left)
+            self._recorrido_preorden(node.right)
+    
+    def imprimir_preorden(self):
+        self._recorrido_preorden(self.root)
         
     def add(self,element,parent=None) -> None:
-        new_nodo=nodo(element,parent)
+        new_nodo=nodo(element)
         if self.weight==0:
             self.root=new_nodo
             self.height+=1
             self.weight+=1
         else:
-            if parent.left is not(None):
-                parent.left=new_nodo
+            father=self.search(parent,self.root)
+            if not(father):
+                raise 'El nodo padre indicado no existe'
+            if father.left is None:
+                father.left=new_nodo
+            elif father.right is None:
+                father.right=new_nodo
             else:
-                parent.right=new_nodo
-            new_nodo.parent=parent
+                raise Exception('El nodo padre ya tiene dos hijos')
+            new_nodo.parent=father
             self.weight+=1
             
-            
+if __name__=='__main__':
+    a1=BinaryTree()
+    a1.add(10)
+    a1.add(2,10)
+    a1.add(3,10)
+    a1.add(7,2)
+    a1.add(5,2)
+    a1.add(1,3)
+    a1.imprimir_preorden()
